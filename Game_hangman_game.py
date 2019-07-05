5e38be217b3da22db13c9e877e175181dad8518f
jungheeyang <50818242+jungheeyang@users.noreply.github.com>
2019-07-05 20:27:53 +1000

    Add messages at the beginning

--- Game_hangman_game.py
+++ Game_hangman_game.py
@@ -19,7 +19,7 @@ for t in words[0]:
     scarps = t.text_content()
     col.append((scarps))
 
-# making a list 
+# making a list
 scarps = " ".join(re.findall("[a-zA-Z]+",scarps.replace('\n','')))
 scarps=list(scarps.split(" "))
 
@@ -28,8 +28,19 @@ scarps=list(scarps.split(" "))
 answer = random.choice(scarps)
 while (len(answer) < 3) and (len(answer)>8):
     answer = random.choice(scarps)
-    
+
 # print(answer)
+
+
+
+print("\n\nLet's play hangman!")
+print("Guess the word character by character!")
+print("You only have 10 chances to save man's life. ")
+print("Good luck! ♣︎\n")
+print(f'The word has {len(answer)} characters')
+
+
+
 
 # FUNCTION chcking if it's valid input
 def right_or_not(char):
@@ -38,12 +49,12 @@ def right_or_not(char):
         if len(char)!=1:
             return False
         return False
-        
+    
     else:
         if len(char)!=1:
             return False
         return True
-    
+
 def hangman_figure(num):
     if num==1:
         print('-----')
@@ -118,12 +129,12 @@ def find_char_position(char,word):
         n +=1
         if x == char:
             return n-1
-        
-# FUNCTION change a character in a certain position  
+
+# FUNCTION change a character in a certain position
 def change_char(s, p, r):
     return s[:p]+r+s[p+1:]
 
-# FUNCTION check the answer 
+# FUNCTION check the answer
 def checking(answer):
     count=1
     guess = '_'*len(answer)
@@ -142,7 +153,7 @@ def checking(answer):
             guess = change_char(guess,n,char)
             if guess == answer:
                 return True
-            
+    
         else:
             print('Wrong guess :(')
             print(f'{10-count} guesses left!\n')
@@ -150,12 +161,12 @@ def checking(answer):
             hangman_figure(count)
             if count==10:
                 return False
-        
-            
+
+                    
         print(f'Your guess >> {guess}')
-        
-            
-        
+
+
+
 if checking(answer):
     print('\nYou have saved a man! Congrats! :D')
     print('    ♥︎')
@@ -165,8 +176,8 @@ if checking(answer):
     print('  |   < Thank you!')
     print(' / \\')
     print('------')
-                    
-    
+
+
 else:
     print('\nGame over.')
 
