5ddc424165df18737d5a01f4bba677a8bd02b12e
jungheeyang <50818242+jungheeyang@users.noreply.github.com>
2019-07-05 20:19:48 +1000

    Update Hangman game
    Change it to accept any right character from the answer in any position. 
    ie. answer = 'heard' and input 'a' without answering 'h' and 'e' beforehand. 
    
    Display of answers

--- Game_hangman_game.py
+++ Game_hangman_game.py
@@ -19,38 +19,31 @@ for t in words[0]:
     scarps = t.text_content()
     col.append((scarps))
 
-# making a list
+# making a list 
 scarps = " ".join(re.findall("[a-zA-Z]+",scarps.replace('\n','')))
 scarps=list(scarps.split(" "))
 
 
-# pick a random word
+# pick a random word ( 3 to 7 characters)
 answer = random.choice(scarps)
-temp_answer = answer
+while (len(answer) < 3) and (len(answer)>8):
+    answer = random.choice(scarps)
+    
 # print(answer)
 
-
-print("\n\nLet's play hangman!")
-print("Guess the word character by character!")
-print("You only have 10 chances to save man's life. ")
-print("Good luck! ♣︎\n")
-print(f'The word has {len(answer)} characters')
-
-
-
-# FUNCTION checking if it's valid input
+# FUNCTION chcking if it's valid input
 def right_or_not(char):
+    
     if not char.isalpha():
-        if len(char) != 1:
+        if len(char)!=1:
             return False
         return False
-
+        
     else:
-        if len(char) != 1:
+        if len(char)!=1:
             return False
         return True
-
-# FUNCTION hangman figure
+    
 def hangman_figure(num):
     if num==1:
         print('-----')
@@ -118,37 +111,51 @@ def hangman_figure(num):
         print('|')
         print('--------')
 
-# FUNCTION check the answer
+# FUNCTION find the char's position in a word
+def find_char_position(char,word):
+    n=0
+    for x in word:
+        n +=1
+        if x == char:
+            return n-1
+        
+# FUNCTION change a character in a certain position  
+def change_char(s, p, r):
+    return s[:p]+r+s[p+1:]
+
+# FUNCTION check the answer 
 def checking(answer):
-    count = 1
-    guess = ''
-
+    count=1
+    guess = '_'*len(answer)
+    
     while True:
-        char = input('What is your guess? \t>> ')
+        n=0
+        char = input('Enter the character ')
         char = char.lower()
-
+        
         if not right_or_not(char):
-            print('Please enter valid input')
-
-        elif char == answer[0]:
-            print('Good guess! :) ')
-            answer = answer[1:]
-            guess = guess + char
-            if guess == temp_answer:
+            print('Please enter valiad input\n')
+        
+        elif char in answer:
+            print('Good guess! :) \n')
+            n = find_char_position(char,answer)
+            guess = change_char(guess,n,char)
+            if guess == answer:
                 return True
-
+            
         else:
             print('Wrong guess :(')
-            print(f'{10 - count} guesses left!')
+            print(f'{10-count} guesses left!\n')
             count += 1
             hangman_figure(count)
-            if count == 10:
+            if count==10:
                 return False
-
+        
+            
         print(f'Your guess >> {guess}')
-
-
-# Game ending
+        
+            
+        
 if checking(answer):
     print('\nYou have saved a man! Congrats! :D')
     print('    ♥︎')
@@ -158,6 +165,8 @@ if checking(answer):
     print('  |   < Thank you!')
     print(' / \\')
     print('------')
-
+                    
+    
 else:
     print('\nGame over.')
+
